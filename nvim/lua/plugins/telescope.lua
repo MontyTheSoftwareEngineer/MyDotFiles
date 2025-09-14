a397c6d87d79c059d3fb6430833f5fa93f14a5ba
Hai Pham <hai.pham@mvebio.com>
2025-09-14 11:15:40 -0400

    Remove old nvim plugins

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
     vim.keymap.set('n', '<leader>k', builtin.find_files, {})
     vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>s', builtin.current_buffer_fuzzy_find, {})
+    vim.keymap.set('n', '<leader>o', builtin.lsp_document_symbols, {})
   end
 }
 
