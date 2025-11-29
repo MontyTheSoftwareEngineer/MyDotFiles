002df9b1fddf74accf11077d88ec7cd07aed728c
Hai Pham <hai.pham@mvebio.com>
2025-11-29 08:36:50 -0500

    Dark theme

--- nvim/lua/plugins/leap.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "ggandor/leap.nvim",
-  config = function()
-    local leap = require('leap')
-    leap.opts.smart_case = true
-  end,
-}
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,65 +2,47 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      -- Setup clangd LSP for C++
-      require'lspconfig'.clangd.setup{
-        on_attach = function(client, bufnr)
-          local function buf_set_keymap(...) 
-            vim.api.nvim_buf_set_keymap(bufnr, ...) 
-          end
-          local opts = { noremap=true, silent=true }
-
-          -- Setup keybindings for LSP features
-          buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)  -- Go to Definition
-          buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)   -- Show References
-          buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)  -- Go to Declaration
-          buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)  -- Go to Implementation
-
-          -- Optionally, setup completion with nvim-cmp
-          require'cmp'.setup.buffer { enabled = true }
+      local function on_attach(client, bufnr)
+        local function buf_set_keymap(...) 
+          vim.api.nvim_buf_set_keymap(bufnr, ...) 
         end
+        local opts = { noremap=true, silent=true }
+
+        -- Setup keybindings for LSP features
+        buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)  -- Go to Definition
+        buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)   -- Show References
+        buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)  -- Go to Declaration
+        buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)  -- Go to Implementation
+
+        -- Optionally, setup completion with nvim-cmp
+        require'cmp'.setup.buffer { enabled = true }
+      end
+
+      -- Setup clangd LSP for C++
+      vim.lsp.config.clangd = {
+        cmd = { 'clangd' },
+        filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
+        on_attach = on_attach,
       }
 
       -- Setup rust_analyzer LSP for Rust
-      require'lspconfig'.rust_analyzer.setup{
-        on_attach = function(client, bufnr)
-          local function buf_set_keymap(...) 
-            vim.api.nvim_buf_set_keymap(bufnr, ...) 
-          end
-          local opts = { noremap=true, silent=true }
-
-          -- Setup keybindings for LSP features
-          buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)  -- Go to Definition
-          buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)   -- Show References
-          buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)  -- Go to Declaration
-          buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)  -- Go to Implementation
-
-          -- Optionally, setup completion with nvim-cmp
-          require'cmp'.setup.buffer { enabled = true }
-        end
+      vim.lsp.config.rust_analyzer = {
+        cmd = { 'rust-analyzer' },
+        filetypes = { 'rust' },
+        on_attach = on_attach,
       }
 
       -- Setup marksman LSP for Markdown
-      require'lspconfig'.marksman.setup{
-        on_attach = function(client, bufnr)
-          local function buf_set_keymap(...) 
-            vim.api.nvim_buf_set_keymap(bufnr, ...) 
-          end
-          local opts = { noremap=true, silent=true }
-
-          -- Setup keybindings for LSP features
-          buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)  -- Go to Definition
-          buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)   -- Show References
-          buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)  -- Go to Declaration
-          buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)  -- Go to Implementation
-
-          -- Optionally, setup completion with nvim-cmp
-          require'cmp'.setup.buffer { enabled = true }
-        end
+      vim.lsp.config.marksman = {
+        cmd = { 'marksman', 'server' },
+        filetypes = { 'markdown', 'markdown.mdx' },
+        on_attach = on_attach,
       }
 
       -- Setup lua_ls for Lua (fixes undefined 'vim' warnings)
-      require'lspconfig'.lua_ls.setup{
+      vim.lsp.config.lua_ls = {
+        cmd = { 'lua-language-server' },
+        filetypes = { 'lua' },
         settings = {
           Lua = {
             diagnostics = {
